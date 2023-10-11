4c0d5e95bbfd769ad0581d0a48034478f1ec0a85
Sheikh Zain Jaffer <136816448+zainjsheikh900@users.noreply.github.com>
2023-10-11 16:09:55 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+const productCart = [
+    { 
+        productName: "Product 1", 
+        price: 10.99 
+    },
+    { 
+        productName: "Product 2", 
+        price: 19.99 
+    },
+    { 
+        productName: "Product 3", 
+        price: 8.50 
+    },
+    { 
+        productName: "Product 4", 
+        price: 12.00 
+    },
+    { 
+        productName: "Product 5", 
+        price: 15.75 
+    },
+    { 
+        productName: "Product 6", 
+        price: 6.99 
+    },
+    { 
+        productName: "Product 7", 
+        price: 24.50 
+    },
+    { 
+        productName: "Product 8", 
+        price: 17.25 
+    },
+    { 
+        productName: "Product 9", 
+        price: 11.49 
+    },
+    { 
+        productName: "Product 10", 
+        price: 14.99 
+    }
+];
+
+const totalValue = productCart.reduce
+
+
